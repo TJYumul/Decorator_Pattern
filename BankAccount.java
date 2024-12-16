13ee2b9ec5659fbd256dec223e7dc7d7fdaeb56a
TJ <132363388+TJYumul@users.noreply.github.com>
2024-12-16 13:57:01 +0800

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,8 @@
+public interface BankAccount {
+    String showAccountType();
+    double getInterestRate();
+    double getBalance();
+    double computeBalanceWithInterest();
+    String showBenefits();
+    String showInfo();
+}
